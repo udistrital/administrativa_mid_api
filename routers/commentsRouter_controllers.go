426b128ade24cc004829259d90d62b69e07476cd
Camilo Torres <camilo@ulises.udistritaloas.edu.co>
2017-08-09 11:24:37 -0500

    Solucion problema caracteres no visibles en commentrouters

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -8,40 +8,35 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:InformacionProveedorController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:InformacionProveedorController"],
 		beego.ControllerComments{
-			Method: "Contrato_proveedor",
-			Router: `/contrato_proveedor`,
+			Method:           "Contrato_proveedor",
+			Router:           `/contrato_proveedor`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
-<<<<<<< HEAD
+			Params:           nil})
+	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"],
+		beego.ControllerComments{
+			Method:           "CalcularSalarioPrecontratacion",
+			Router:           `Precontratacion/:nivelAcademico/:idProfesor/:numHoras/:numSemanas/:categoria/:dedicacion`,
+			AllowHTTPMethods: []string{"post"},
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"],
 		beego.ControllerComments{
-			Method: "CalcularSalarioPrecontratacion",
-			Router: `Precontratacion/:nivelAcademico/:idProfesor/:numHoras/:numSemanas/:categoria/:dedicacion`,
-			AllowHTTPMethods: []string{"post"},
-			Params: nil})
-
-	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CalculoSalarioController"],
-		beego.ControllerComments{
-			Method: "CalcularSalarioContratacion",
-			Router: `Contratacion/:idVinculacion`,
+			Method:           "CalcularSalarioContratacion",
+			Router:           `Contratacion/:idVinculacion`,
 			AllowHTTPMethods: []string{"get"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:ValidarContratoController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:ValidarContratoController"],
 		beego.ControllerComments{
-			Method: "ValidarContrato",
-			Router: `/:dedicacion/:numHoras`,
+			Method:           "ValidarContrato",
+			Router:           `/:dedicacion/:numHoras`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
+			Params:           nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CancelacionValidaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:CancelacionValidaController"],
 		beego.ControllerComments{
-			Method: "ValidarCancelacion",
-			Router: `/:idResolucion`,
+			Method:           "ValidarCancelacion",
+			Router:           `/:idResolucion`,
 			AllowHTTPMethods: []string{"post"},
-			Params: nil})
-
-=======
->>>>>>> issue/2
+			Params:           nil})
 }
