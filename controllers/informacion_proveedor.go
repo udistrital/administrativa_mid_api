691e5687b03e14f6db20c08b1ed9d1fbee6ccc63
docg92 <diegoorlando5@hotmail.com>
2017-08-15 15:32:11 -0500

    agregando if para evitar el desbordamiento del arreglo

--- controllers/informacion_proveedor.go
+++ controllers/informacion_proveedor.go
@@ -36,17 +36,19 @@ func (c *InformacionProveedorController) Contrato_proveedor() {
 		for x := 0; x < len(datos); x++ {
 
 			cedula := strconv.Itoa(datos[x].Contratista)
-
 			queryPersonaNatural := "?query=Id:"+cedula
 			queryInformacionProveedor := "?query=NumDocumento:"+cedula
-
 			if err := getJson("http://"+beego.AppConfig.String("UrlcrudAgora")+"/"+beego.AppConfig.String("NscrudArgo")+"/informacion_persona_natural/"+queryPersonaNatural, &persona_natural); err == nil {
 			if err2 := getJson("http://"+beego.AppConfig.String("UrlcrudAgora")+"/"+beego.AppConfig.String("NscrudArgo")+"/informacion_proveedor/"+queryInformacionProveedor, &informacion_proveedor); err2 == nil {
 
-				temp.InformacionProveedor = informacion_proveedor[0]
-				temp.InformacionPersonaNatural = persona_natural[0]
-				temp.ContratoGeneral = datos[x]
-				contrato_proveedor = append(contrato_proveedor, temp)
+				if informacion_proveedor != nil && persona_natural != nil{
+					temp.InformacionProveedor = informacion_proveedor[0]
+					temp.InformacionPersonaNatural = persona_natural[0]
+					temp.ContratoGeneral = datos[x]
+					contrato_proveedor = append(contrato_proveedor, temp)
+				}
+
+
 
 				c.Data["json"] = contrato_proveedor
 			}else {
