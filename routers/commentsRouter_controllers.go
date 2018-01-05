b1e2057f9897d2202db39f3d5f07aed118d9dbb7
Maria Alejandra Ortiz <mariaalejandra9404@gmail.com>
2018-01-05 00:17:26 -0500

    se corrige estructura que se dañó en merge

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -73,16 +73,16 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"],
 		beego.ControllerComments{
-			Method: "Calcular_total_de_salarios",
-			Router: `Precontratacion/calcular_valor_contratos`,
-			AllowHTTPMethods: []string{"post"},
+			Method: "ListarDocentesPrevinculados",
+			Router: `/docentes_previnculados`,
+			AllowHTTPMethods: []string{"get"},
 			MethodParams: param.Make(),
 			Params: nil})
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"],
 		beego.ControllerComments{
-			Method: "InsertarPrevinculaciones",
-			Router: `Precontratacion/insertar_previnculaciones`,
+			Method: "Calcular_total_de_salarios",
+			Router: `Precontratacion/calcular_valor_contratos`,
 			AllowHTTPMethods: []string{"post"},
 			MethodParams: param.Make(),
 			Params: nil})
@@ -97,9 +97,9 @@ func init() {
 
 	beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"] = append(beego.GlobalControllerRouter["github.com/udistrital/administrativa_mid_api/controllers:GestionPrevinculacionesController"],
 		beego.ControllerComments{
-			Method: "ListarDocentesPrevinculados",
-			Router: `/docentes_previnculados`,
-			AllowHTTPMethods: []string{"get"},
+			Method: "InsertarPrevinculaciones",
+			Router: `Precontratacion/insertar_previnculaciones`,
+			AllowHTTPMethods: []string{"post"},
 			MethodParams: param.Make(),
 			Params: nil})
 
