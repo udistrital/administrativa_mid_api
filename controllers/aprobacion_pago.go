2e7a4c22185ace0fc1c8ae2822d5dc8c11b6f5d9
Javier Reyes <ingjsreyesm@gmail.com>
2018-01-30 15:30:39 -0500

    Servicio que obtiene la información del coordinador

--- controllers/aprobacion_pago.go
+++ controllers/aprobacion_pago.go
@@ -30,6 +30,7 @@ func (c *AprobacionPagoController) ObtenerInfoCoordinador() {
 	id_oikos := c.GetString(":id_dependencia_oikos")
 	var temp map[string]interface{}
 	var temp_snies map[string]interface{}
+	var info_coordinador models.InformacionCoordinador
 
 	if err := getJsonWSO2("http://jbpm.udistritaloas.edu.co:8280/services/servicios_homologacion_dependencias/proyecto_curricular_oikos/"+id_oikos, &temp); err == nil && temp != nil {
 		json_proyecto_curricular, error_json := json.Marshal(temp)
@@ -37,21 +38,16 @@ func (c *AprobacionPagoController) ObtenerInfoCoordinador() {
 		if error_json == nil {
 			var temp_homologacion models.ObjetoProyectoCurricular
 			json.Unmarshal(json_proyecto_curricular, &temp_homologacion)
-			//fmt.Println(temp_homologacion.Homologacion.IDSnies)
 			id_proyecto_snies := temp_homologacion.Homologacion.IDSnies
-			fmt.Println("Este " + id_proyecto_snies)
 
-			if err := getJsonWSO2("http://jbpm.udistritaloas.edu.co:8280/services/academicaProxy/carrera_snies/918", &temp_snies); err == nil && temp_snies != nil {
+			if err := getJsonWSO2("http://jbpm.udistritaloas.edu.co:8280/services/academicaProxy/carrera_snies/"+id_proyecto_snies, &temp_snies); err == nil && temp_snies != nil {
 				json_info_coordinador, error_json := json.Marshal(temp_snies)
-
-				fmt.Println(json_info_coordinador)
 
 				if error_json == nil {
 					var temp_info_coordinador models.InformacionCoordinador
 					json.Unmarshal(json_info_coordinador, &temp_info_coordinador)
 					fmt.Println(temp_info_coordinador)
-					//id_proyecto_snies = temp_homologacion.Homologacion.IDSnies
-
+					info_coordinador = temp_info_coordinador
 				} else {
 					fmt.Println(error_json.Error())
 					// c.Data["json"] = error_json.Error()
@@ -67,4 +63,6 @@ func (c *AprobacionPagoController) ObtenerInfoCoordinador() {
 
 	}
 
+	c.Data["json"] = info_coordinador
+	c.ServeJSON()
 }
--- models/carrera_snies.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type CarreraSnies struct {
-	NombreCoordinador          string `json:"nombre_coordinador"`
-	NumeroDocumentoCoordinador string `json:"numero_documento_coordinador"`
-	NombreProyectoCurricular   string `json:"nombre_proyecto_curricular"`
-}
