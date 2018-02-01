52f9b37fdb655c91d188bf905f4eb02666860003
Kmilo9433 <camilo@ulises.udistritaloas.edu.co>
2018-01-31 21:53:10 -0500

    Servicio contratos_docente

--- controllers/aprobacion_pago.go
+++ controllers/aprobacion_pago.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/astaxie/beego"
 	"github.com/udistrital/administrativa_mid_api/models"
@@ -16,6 +17,7 @@ type AprobacionPagoController struct {
 // URLMapping ...
 func (c *AprobacionPagoController) URLMapping() {
 	c.Mapping("ObtenerInfoCoordinador", c.ObtenerInfoCoordinador)
+	c.Mapping("GetContratosDocente", c.GetContratosDocente)
 
 }
 
@@ -64,5 +66,72 @@ func (c *AprobacionPagoController) ObtenerInfoCoordinador() {
 	}
 
 	c.Data["json"] = info_coordinador
+	c.ServeJSON()
+}
+
+// AprobacionPagoController ...
+// @Title GetContratosDocente
+// @Description create  GetContratosDocente
+// @Param numDocumento query string true "Docente a consultar"
+// @Success 201 {object} []models.ContratosDocentes
+// @Failure 403 body is empty
+// @router /get_contratos_docente [get]
+func (c *AprobacionPagoController) GetContratosDocente() {
+	numDocumento := c.GetString("numDocumento")
+	var contratosDocentes []models.ContratosDocente
+	var cd models.ContratosDocente
+	var proveedor []models.InformacionProveedor
+	var vinculaciones []models.VinculacionDocente
+	//var contrato []models.ContratoGeneral
+	var contratoEstado []models.ContratoEstado
+	var res models.Resolucion
+	var dep models.Dependencia
+	//If informacion_proveedor get
+	if err := getJson("http://"+beego.AppConfig.String("UrlcrudAgora")+"/"+beego.AppConfig.String("NscrudAgora")+"/informacion_proveedor/?query=num_documento:"+numDocumento, &proveedor); err == nil {
+		//If vinculacion_docente get
+		if err := getJson("http://"+beego.AppConfig.String("UrlcrudAdmin")+"/"+beego.AppConfig.String("NscrudAdmin")+"/vinculacion_docente/?query=id_persona:"+numDocumento, &vinculaciones); err == nil {
+			//for vinculaciones
+			for _, vinculacion := range vinculaciones {
+				//If dependencia get
+				if err := getJson("http://"+beego.AppConfig.String("UrlcrudOikos")+"/"+beego.AppConfig.String("NscrudOikos")+"/dependencia/"+strconv.Itoa(vinculacion.IdProyectoCurricular), &dep); err == nil {
+					//If resolucion get
+					if err := getJson("http://"+beego.AppConfig.String("UrlcrudAdmin")+"/"+beego.AppConfig.String("NscrudAdmin")+"/resolucion/"+strconv.Itoa(vinculacion.IdResolucion.Id), &res); err == nil {
+						//If nulo
+						if vinculacion.NumeroContrato.Valid == true {
+							if err := getJson("http://"+beego.AppConfig.String("UrlcrudAgora")+"/"+beego.AppConfig.String("NscrudAgora")+"/contrato_estado/?query=NumeroContrato:"+vinculacion.NumeroContrato.String+",Vigencia:"+strconv.FormatInt(vinculacion.Vigencia.Int64, 10)+"&sortby=FechaRegistro&order=desc&limit=1", &contratoEstado); err == nil {
+								//If Estado = 4
+								if contratoEstado[0].Estado.Id == 4 {
+									cd.NumeroVinculacion = vinculacion.NumeroContrato.String
+									cd.Vigencia = vinculacion.Vigencia.Int64
+									cd.Resolucion = res.NumeroResolucion
+									cd.Dependencia = dep.Nombre
+									cd.IdDependencia = dep.Id
+									cd.NombreDocente = proveedor[0].NomProveedor
+									cd.Dedicacion = vinculacion.IdDedicacion.NombreDedicacion
+									contratosDocentes = append(contratosDocentes, cd)
+								}
+							} else { //If contrato_estado get
+								fmt.Println("Mirenme, me morí en If contrato_estado get, solucioname!!! ", err)
+								return
+							}
+						}
+					} else { //If resolucion get
+						fmt.Println("Mirenme, me morí en If resolucion get, solucioname!!! ", err)
+						return
+					}
+				} else { //If dependencia get
+					fmt.Println("Mirenme, me morí en If dependencia get, solucioname!!! ", err)
+					return
+				}
+			} //for vinculaciones
+			c.Data["json"] = contratosDocentes
+		} else { //If informacion_proveedor get
+			fmt.Println("Mirenme, me morí en informacion proveedor, solucioname!!! ", err)
+			return
+		}
+	} else { //If informacion_proveedor get
+		fmt.Println("Mirenme, me morí en informacion proveedor, solucioname!!! ", err)
+		return
+	}
 	c.ServeJSON()
 }
