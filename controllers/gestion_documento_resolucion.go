b2ed47fb5887e100aeb5bb83000a8d76f84b7ef8
JocoolQ <jscaperaq@correo.udistrital.edu.co>
2021-12-29 20:46:10 -0500

    fix: prueba cuadro responsabilidades

--- controllers/gestion_documento_resolucion.go
+++ controllers/gestion_documento_resolucion.go
@@ -36,7 +36,7 @@ func (c *GestionDocumentoResolucionController) GetContenidoResolucion() {
 
 	if err2 := getJson(beego.AppConfig.String("ProtocolAdmin")+"://"+beego.AppConfig.String("UrlcrudAdmin")+"/"+beego.AppConfig.String("NscrudAdmin")+"/contenido_resolucion/"+id_resolucion, &contenidoResolucion); err2 == nil {
 		query = "?limit=-1&query=DependenciaId:" + id_facultad
-		fmt.Println(contenidoResolucion.CuadroResponsabilidades)
+		fmt.Printf("%+v\n", contenidoResolucion)
 		if err := getJson(beego.AppConfig.String("ProtocolAdmin")+"://"+beego.AppConfig.String("UrlcrudCore")+"/"+beego.AppConfig.String("NscrudCore")+"/ordenador_gasto/"+query, &ordenador_gasto); err == nil {
 			if ordenador_gasto == nil {
 				if err := getJson(beego.AppConfig.String("ProtocolAdmin")+"://"+beego.AppConfig.String("UrlcrudCore")+"/"+beego.AppConfig.String("NscrudCore")+"/ordenador_gasto/1", &ordenador_gasto); err == nil {
@@ -49,6 +49,7 @@ func (c *GestionDocumentoResolucionController) GetContenidoResolucion() {
 			}
 
 		} else {
+			fmt.Println(beego.AppConfig.String("UrlcrudAdmin"))
 
 			fmt.Println("Error al consultar ordenador del gasto", err)
 		}
